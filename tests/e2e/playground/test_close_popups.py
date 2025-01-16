d266cecdc43744310e56fdfc1316a47b65a1390f
JanetziJackiewicz <janetzi.jackiewicz@gmail.com>
2025-01-15 22:24:05 -0400

    config pipeline for testing

--- tests/e2e/playground/test_close_popups.py
+++ tests/e2e/playground/test_close_popups.py
@@ -2,21 +2,16 @@ import pytest
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 
+
 class TestClosePopups:
     def test_should_display_message_when_closing_popup(self):
         web = Chrome()
         web.get('https://qaplayground.dev/apps/onboarding-modal/#')
-        menu_button = web.find_element(By.CSS_SELECTOR,'.menu-btn')
+        menu_button = web.find_element(By.CSS_SELECTOR, '.menu-btn')
         menu_button.click()
-        message_final = web.find_element(By.CSS_SELECTOR,'[class="title"]').text
+        message_final = web.find_element(By.CSS_SELECTOR, '[class="title"]').text
         assert 'Welcome Peter Parker!' in message_final
 
-        
 
-
-
-
-
-
-if __name__ == "__main__":  
-    pytest.main()   
+if __name__ == "__main__":
+    pytest.main()
