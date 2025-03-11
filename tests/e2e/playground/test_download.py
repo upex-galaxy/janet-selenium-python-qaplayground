bf990fcbddd8bd25b1dbec96b0d2dc6b2cdeb05e
JanetziJackiewicz <janetzi.jackiewicz@gmail.com>
2025-03-11 19:37:46 -0400

    Fix: se creo ruta de descargar temporal con chrome options y default_directory

--- tests/e2e/playground/test_download.py
+++ tests/e2e/playground/test_download.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from tests.pages.page_dowload import TestDownloadPage
@@ -7,18 +8,29 @@ from tests.pages.page_dowload import TestDownloadPage
 
 class TestDownload:
     def test_should_dowload_a_file_successfully(self):
-        driver = webdriver.Chrome()
+
+        download_directory = os.path.abspath('./tmp')
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {'download.default_directory': download_directory}
+        chrome_options.add_experimental_option('prefs', prefs)
+        driver = webdriver.Chrome(options=chrome_options)
         driver.get('https://qaplayground.dev/apps/download/')
+
         test_download = TestDownloadPage(driver)
         test_download.download_file()
-        download_dirs = "C:\\Users\\jjack\\Downloads"
-        nombre_archivo = "sample.pdf"
-        archivos = os.listdir(download_dirs)
-        archivos_completos = [os.path.join(download_dirs, archivo) for archivo in archivos]
+
+        file_name = "sample.pdf"
+
+        archivos = os.listdir(download_directory)
+        archivos_completos = [os.path.join(download_directory, archivo) for archivo in archivos]
         archivos_ordenados = sorted(archivos_completos, key=os.path.getmtime, reverse=True)
-        ultimas_descargas = archivos_ordenados[:10]
+        ultimas_descargas = archivos_ordenados[:1]
+        ruta_del_archivo = os.path.join(download_directory, ultimas_descargas)
         nombres_archivos = [os.path.basename(ruta) for ruta in ultimas_descargas]
-        assert nombre_archivo in nombres_archivos
+
+        assert file_name in nombres_archivos
+
+        os.remove(ruta_del_archivo)
 
 
 if __name__ == "__main__":
