7208cd3df45b919ab91a786f46583acb0c9c2fef
JanetziJackiewicz <janetzi.jackiewicz@gmail.com>
2025-03-18 19:14:07 -0400

    se crea el ultimo caso de prueba

--- tests/e2e/playground/test_tags.py
+++ tests/e2e/playground/test_tags.py
@@ -10,15 +10,19 @@ class TestTagsInputBox:
         tagsinput = TestTagsInputBoxPage(driver)
         tagsinput.add_tags_in_container()
 
-    # def test_should_remove_a_tag_successfully(self):
+    def test_should_remove_a_tag_successfully(self):
+        driver = webdriver.Chrome()
+        driver.get('https://qaplayground.dev/apps/tags-input-box/')
+        delete_a_tag = TestTagsInputBoxPage(driver)
+        delete_a_tag.remove_a_tag_in_container()
 
     def test_should_remove_all_tag_successfully(self):
         driver = webdriver.Chrome()
         driver.get('https://qaplayground.dev/apps/tags-input-box/')
-        tagsinput = TestTagsInputBoxPage(driver)
-        tagsinput.delete_all_tags_in_container
-        conteniner_tags = tagsinput.message.text
-        assert conteniner_tags == 10
+        delete_all_tags = TestTagsInputBoxPage(driver)
+        delete_all_tags.delete_all_tags_in_container()
+        conteniner_tags = delete_all_tags.message.text
+        assert conteniner_tags == '10'
 
 
 if __name__ == "__main__":
--- tests/pages/page_tags.py
+++ tests/pages/page_tags.py
@@ -12,8 +12,9 @@ class TestTagsInputBoxPage:
     def __post_init__(self):
         self.container_tags = self.driver.find_elements(By.CSS_SELECTOR, '.content li')
         self.input_file = self.driver.find_element(By.CSS_SELECTOR, '.content input')
-        self.button_delete_all = self.driver.find_element(By.CSS_SELECTOR, '.details button')
         self.message = self.driver.find_element(By.CSS_SELECTOR, '.details span')
+        self.remove_all_button_locator = (By.CSS_SELECTOR, '.details button')
+        self.remove_a_tags_locator = self.driver.find_element(By.TAG_NAME, 'i')
 
     def add_tags_in_container(self):
         tabs_count = len(self.container_tags)
@@ -27,14 +28,11 @@ class TestTagsInputBoxPage:
                 self.input_file.send_keys(Keys.ENTER)
 
     def delete_all_tags_in_container(self):
-        button_delete = self.button_delete_all
+        button_delete = self.driver.find_element(*self.remove_all_button_locator)
         button_delete.click()
 
     def remove_a_tag_in_container(self):
         tabs_count = len(self.container_tags)
         if tabs_count > 0:
-            index_tabs = random.randint(1, tabs_count)
-
-            for index in range(index_tabs):
-                self.input_file.send_keys(elements_tags[index])
-                self.input_file.send_keys(Keys.ENTER)
+            delete_a_tag_ac = self.container_tags[-1].find_element(By.TAG_NAME, "i")
+            delete_a_tag_ac.click()
